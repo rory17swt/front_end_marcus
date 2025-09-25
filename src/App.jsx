63afc525bc293de58b8fe6fe85b1afcb07ad9046
rory_swt <rory@swietlicki.eu>
2025-09-25 14:26:14 +0100

    App.jsx set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,33 @@
 import { Routes, Route } from "react-router"
 
+import Home from './components/Home/Home'
+import Media from './components/Media/Media'
+import Contact from './components/Contact/Contact'
+import AdminLogin from './components/AdminLogin/AdminLogin'
+import BioForm from './components/BioForm/BioForm'
+import EventsForm from './components/EventsForm/EventsForm'
+import MediaForm from './components/MediaForm/MediaForm'
+import Navbar from './components/Navbar/Navbar'
+import Footer from './components/Footer/Footer'
+
 
 function App() {
-  return <h1>My App</h1>  
+  return (
+    <>
+    <h1 Marcus Portfolio></h1>
+    <Navbar />
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/media' element={<Media />} />
+      <Route path='/contact' element={<Contact />} />
+      <Route path='/adminLogin' element={<AdminLogin />} />
+      <Route path='/bioForm' element={<BioForm />} />
+      <Route path='/eventsForm' element={<EventsForm />} />
+      <Route path='/mediaForm' element={<MediaForm />} />
+    </Routes>
+    <Footer />
+    </>
+  )
 }
 
 
