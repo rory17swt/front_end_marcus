a57cd49795a9728b02781120369eacc4a6799964
rory_swt <rory@swietlicki.eu>
2025-11-17 15:40:13 +0000

    Added spinner

--- src/components/Spinner/Spinner.jsx
+++ src/components/Spinner/Spinner.jsx
@@ -1,3 +1,9 @@
-export default function Spinner() {
-    return <h2>Spinner</h2> 
+import ClipLoader from "react-spinners/ClipLoader"
+
+export default function Spinner({ loading }) {
+    return (
+        <div className="flex items-center justify-center w-screen h-screen">
+            <ClipLoader color="#C4A77D" loading={loading} size={50} />
+        </div>
+    )
 }
